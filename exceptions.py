71c5a3272b79418da6fb766aaa50d6dd2d49f8c4
Askhat <askat.amandykov@nu.edu.kz>
2026-09-04 18:43:30 +0500

    Add exceptions

---
+++ exceptions.py
@@ -0,0 +1,26 @@
+class UserAlreadyExistsError(Exception):
+    pass
+
+
+class UserNotFoundError(Exception):
+    pass
+
+
+class InvalidCredentialsError(Exception):
+    pass
+
+
+class RoomNotFoundError(Exception):
+    pass
+
+
+class BookingNotFoundError(Exception):
+    pass
+
+
+class BookingConflictError(Exception):
+    pass
+
+
+class BookingAccessDeniedError(Exception):
+    pass
